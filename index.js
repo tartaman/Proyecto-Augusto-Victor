b0060027c73393dde22a56491f85f794d777df9b
tartaman <popolol5342@gmail.com>
2021-02-17 20:09:15 -0600

    cambio 2
    empezamos

---
+++ index.js
@@ -0,0 +1,46 @@
+/*function Proyecto() {
+    const escoge = prompt("Escoge cuantos sets quieres jugar");
+    if (sets > 3){
+        alert("por favor escoge menos de 3 sets")
+        Proyecto()
+    }
+    if (sets <= 3){
+
+    const escogeMaquina = Math.floor(Math.random()* 5)
+    if (escogeMaquina <= 0){
+        numeroEscogido++
+    }
+    if (escogeMaquina == 1){
+        escogeMaquina = "Piedra"
+    }
+    if(escoge == null){
+    } else { if (escoge > 5) {
+           alert("por favor ingresa un número válido")
+           miFuncion()
+       } else {
+           if (escoge < 1){
+               alert("por favor ingresa un número válido")
+               miFuncion()
+           } else {
+               if (escoge.length > 1){
+                   alert("por favor ingresa un número válido")
+                   miFuncion()
+                   } else {
+                   if (escoge == escogeMaquina) {
+                       alert(`Empatamos, tu escogiste ${escoge} y yo ${escogeMaquina}`)
+                   }  else  {
+                       alert(`Diablos, no lo lograste, mejor suerte la proxima :D, yo escogí ${numeroEscogido} y tu escogiste ${numero}`)
+                       var intento = prompt("Intentar de nuevo? \n 1.Si \n 2.No")
+                       if (intento == 1){
+                           miFuncion()
+                       } else {
+                           alert("recarga la página si quieres intentar de nuevo")
+                       }
+                   }
+               }
+           }
+       }
+    }
+}
+}
+Proyecto()*/
